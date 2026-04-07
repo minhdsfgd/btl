c4121a9c840f695d7e3ea478717f8aa8d3589db3
Minhbecohet <dangngocminh207@gmail.com>
2026-04-07 11:00:31 +0700

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        Solution sl = new Solution();
-        System.out.println(sl.gcd(5,10));
-        System.out.println(sl.gcd(-8,20));
-        System.out.println(sl.gcd(-10,-55));
-        System.out.println(sl.gcd(90,0));
-    }
-}
