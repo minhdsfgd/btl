43946e0019d2fb3037abed2390f823c3d89277df
Phan Minh <phanminh10607@gmail.com>
2026-04-18 23:39:13 +0700

    Add some features

--- BaiTapLon/src/main/java/com/code/Auction.java
+++ BaiTapLon/src/main/java/com/code/Auction.java
@@ -1,7 +1,25 @@
 package com.code;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Auction{
+    private String auctionId;
     private Item item;
+    private AuctionStatus status;
+    private double currnetPrice;
+    private Bidder currentWinner;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private List<Bid> bids;
+    private ReentrantLock lock;
+    private List<AuctionObserver> observers;
+
+    public void start(){}
+    public void finish(){}
+    public boolean placeBid(Bidder bidder, double amount){}
+    public void addObservers(AuctionObserver observer){}
+    public void notifyObserers(Bid bid){}
+    public AuctionStatus getStatus(){}
+    public void transitionState(AuctionStatus newStatus){}
 }
--- BaiTapLon/src/main/java/com/code/AuctionManager.java
+++ BaiTapLon/src/main/java/com/code/AuctionManager.java
@@ -1,11 +1,39 @@
 package com.code;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class AuctionManager{
     private static AuctionManager instance;
-    private AuctionManager(){}
+    private Map<String, Auction> auctionMap;
+    private ReentrantLock managerLock;
+
+    private AuctionManager(){
+        this.auctionMap = new HashMap<>();
+        this.managerLock = new ReentrantLock();
+    }
+
     public static AuctionManager getInstance(){
         if (instance == null){
             instance = new AuctionManager();
         }
         return instance;
     }
+    public Auction createAuction(Item item, Seller seller){
+        managerLock.lock();
+        try{
+            Auction auction = new Auction(item, seller);
+            auctionMap.put(auction.getAuctionId(), auction);
+            return auction;
+        }
+        finally {
+            managerLock.unlock();
+        }
+        return new Auction();
+    }
+    public Auction getAuction(String id){}
+    public List<Auction> getAllActive(){}
+    public void closeExpiredAuctions(){};
 }
