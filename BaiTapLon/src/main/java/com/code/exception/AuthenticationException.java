3a25f0a7592f3ce081a85b8c4cd72314b050d0af
minhdsfgd <minhdsfgd1310@gmail.com>
2026-05-03 21:24:10 +0700

    add exception and dependency mysql

---
+++ BaiTapLon/src/main/java/com/code/exception/AuthenticationException.java
@@ -0,0 +1,5 @@
+package com.code.exception;
+public class AuthenticationException extends Exception {
+    public AuthenticationException(String message) { super(message); }
+}
+
---
+++ BaiTapLon/src/main/java/com/code/exception/InvalidBidException.java
@@ -0,0 +1,6 @@
+package com.code.exception;
+public class InvalidBidException extends Exception {
+    public InvalidBidException(String message) {
+        super(message);
+    }
+}
